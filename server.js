de28029ff820e1a50c2004cf4297f7f76ef67109
Quoc Ngo <zanengo04@gmail.com>
2020-08-09 16:50:13 +0700

    added register function

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const bcrypt= require('bcrypt')
+
+
 
 //use database for user instead later
-const user= []
+const users= []
 
 // set view engine to ejs
 app.set("view engine", "ejs");
@@ -37,8 +40,26 @@ app.get('/register', (req,res) =>{
     res.render('register.ejs')
 })
 //post to register page
-app.post('/register', (req,res) =>{
-
+app.post('/register', async (req,res) =>{
+    //create a new user with the correct hashed password
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            // if you have db then you don't need to worry about this. Maybe replace this later 
+            id: Date.now().toString(),
+            name: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        //if the registration was successful then redirect the user
+        res.redirect('/login')
+        
+    } catch {
+        // if there is a failure then...
+        res.redirect('/register')
+        
+    }
+    console.log(users)
 })
 
 //set up port on port 3000
