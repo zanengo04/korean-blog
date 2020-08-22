5879032d32abf81a57cce41dd418f3a5749815f2
Quoc Ngo <zanengo04@gmail.com>
2020-08-21 20:48:51 -0400

    react added to nav bar

---
+++ my-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Nav from './Nav.js';
+function App() {
+  return (
+    <Nav />
+  );
+}
+
+export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App';
 
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <App />,
   document.getElementById('root')
 );
 
