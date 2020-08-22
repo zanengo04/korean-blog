de6a30ab35deafbf8464f7ab841415f5578375bd
Quoc Ngo <zanengo04@gmail.com>
2020-08-22 12:42:19 -0400

    save

--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -8,19 +8,19 @@ export default function Nav() {
                 <ul>
                     <List link="index.html" content="Home"/>
                     <li class="listHover"><a href="#">Start Learning</a>
-                    <ul class='dropMenu'>
-                        <List link="#" content="Learn The Alphabet"/>
-                        <List link="#" content="Basic Grammar"/>
-                        <List link="#" content="Intermediate Grammar"/>
-                        <List link="#" content="Essential Phrases"/>
-                    </ul>
+                        <ul class='dropMenu'>
+                            <List link="#" content="Learn The Alphabet"/>
+                            <List link="#" content="Basic Grammar"/>
+                            <List link="#" content="Intermediate Grammar"/>
+                            <List link="#" content="Essential Phrases"/>
+                        </ul>
                     </li>
                     <li><a href="#">Blog</a></li>
                     <li class="listHover"><a href="typing.html">Typing Practice</a>
-                    <ul class='dropMenu'>
-                        <List link="hangeul.html" content="Hangeul"/>
-                        <List link="paragraph.html" content="Paragraph"/>
-                    </ul>
+                        <ul class='dropMenu'>
+                            <List link="hangeul.html" content="Hangeul"/>
+                            <List link="paragraph.html" content="Paragraph"/>
+                        </ul>
                     </li>
                     <List link="about.html" content="About"/>
                     <List link="contact.html" content="Contact"/>
