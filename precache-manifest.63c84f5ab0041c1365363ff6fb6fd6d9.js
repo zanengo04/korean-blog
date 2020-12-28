95abfc6f6664039342d93bc1c7b3cd4f29061376
Quoc Ngo <zanengo04@gmail.com>
2020-12-28 16:46:48 -0500

    Updates

---
+++ precache-manifest.63c84f5ab0041c1365363ff6fb6fd6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc325b8137000d330c8b076a69a8df50",
+    "url": "/korean-blog/client/public/index.html"
+  },
+  {
+    "revision": "81370295be521b58ffec",
+    "url": "/korean-blog/client/public/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "81370295be521b58ffec",
+    "url": "/korean-blog/client/public/static/js/2.f7b3aaa2.chunk.js"
+  },
+  {
+    "revision": "5727441246ffa49345a3574f984c2034",
+    "url": "/korean-blog/client/public/static/js/2.f7b3aaa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccbd2931851b98ba331",
+    "url": "/korean-blog/client/public/static/js/main.4cf0439b.chunk.js"
+  },
+  {
+    "revision": "fe89d593f6fe825ee196",
+    "url": "/korean-blog/client/public/static/js/runtime-main.e8b98a94.js"
+  }
+]);
